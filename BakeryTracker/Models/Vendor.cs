3b841c50dd3cf4c8f15bee593bb385ede9475a52
ChrisRDavila <crossdavila@gmail.com>
2024-01-26 14:08:54 -0800

    test to find vendor instance from vendor List based on unique id passed

--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -26,6 +26,11 @@ namespace BakeryTracker.Models
     {
       _instances.Clear();
     }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
     
   }
 }
