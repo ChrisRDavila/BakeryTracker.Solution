29a963186fc9a45dad9550081214f5066fc02f69
ChrisRDavila <crossdavila@gmail.com>
2024-01-26 12:47:07 -0800

    Good fail for test 2 get title

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -11,8 +11,17 @@ namespace BakeryTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreateInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("title");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsOrderTitle_String()
+    {
+      string title = "Bread";
+      Order newOrder = new Order(title);
+      string result = newOrder.Title;
+      Assert.AreEqual(title, result);
     }
   }
 }    
