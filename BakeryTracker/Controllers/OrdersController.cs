f7fd4688375b6c4844417bf32562c18658e620b3
ChrisRDavila <crossdavila@gmail.com>
2024-01-26 14:57:23 -0800

    set up ordercontroller get method to return view for form for when creating new order along with form to post new order on submission

--- BakeryTracker/Controllers/OrdersController.cs
+++ BakeryTracker/Controllers/OrdersController.cs
@@ -1,1 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryTracker.Models;
+using System.Collections.Generic;
 
+namespace BakeryTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+  }
+}
