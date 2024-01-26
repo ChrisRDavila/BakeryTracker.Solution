019e540d3a66bf4ae41520345de4d4d00c0a41c1
ChrisRDavila <crossdavila@gmail.com>
2024-01-26 13:16:40 -0800

    test passed or set order date

--- BakeryTracker/Models/Order.cs
+++ BakeryTracker/Models/Order.cs
@@ -7,7 +7,7 @@ namespace BakeryTracker.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public DateTime Date { get; }
+    public DateTime Date { get; set; }
     public Order(string title, string description, decimal price, DateTime date)
     {
       Title = title;
